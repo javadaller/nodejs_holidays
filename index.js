d2abb56482cb09262afd002ca1ce954c07fa0a10
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-07-01 15:13:37 +0200

    okkkkk

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 #!/usr/bin/env node
 
+const axios = require('axios');
 const { getCode } = require('country-list');
-const Holidays = require('date-holidays');
-
-const hd = new Holidays();
 
 const args = process.argv.slice(2);
-if (args.length !== 1) {
-    console.error('Usage: holidates <countryName>');
-    process.exit(1);
-}
 
 const countryName = args[0];
-
 const countryCode = getCode(countryName);
 if (!countryCode) {
     console.error('Invalid country name.');
     process.exit(1);
 }
 
-hd.init(countryCode);
+let year = args[1]
 
-const currentYear = new Date().getFullYear();
-
-const result = hd.getHolidays(currentYear);
-
-for (let i = 0; i < result.length; i++) {
-    console.log(result[i].name);
-    console.log('from ' + result[i].start.toLocaleDateString() + ' to ' + result[i].end.toLocaleDateString());
-    console.log('-----------------------------------');
+if(!year) {
+    year = new Date().getFullYear();
 }
+
+const apiUrl = `https://date.nager.at/api/v3/publicholidays/${year}/${countryCode}`;
+
+axios.get(apiUrl)
+    .then(response => {
+        const holidays = response.data;
+
+        holidays.forEach(holiday => {
+            console.log(holiday.name);
+            console.log(holiday.date);
+            console.log('-----------------------------------');
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching holidays:', error.message);
+    });
